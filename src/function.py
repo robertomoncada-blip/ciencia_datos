1ccb7269c9ba9ef1850a658094b777ddba581694
robertomoncada-blip <roberto.moncada@gmail.com>
2026-06-04 18:06:07 -0400

    Creación notebook

--- src/function.py
+++ src/function.py
@@ -1,9 +1,9 @@
-import panda as pd
+import pandas as pd
 
 def cargar_datos(ruta):
     """Lee el dataset y devuelve un DataFrame."""
     return pd.read_csv(ruta)
 
-df = cargar_datos('data/raw/AI_Impact_on_Jobs_2030.csv')
-print(df.shape, df.dtypes)
+#df = cargar_datos('data/raw/AI_Impact_on_Jobs_2030.csv')
+#print(df.shape, df.dtypes)
 
