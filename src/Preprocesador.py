69020a6f5fcb08aeaf690eaa38f64207de5efcee
robertomoncada-blip <roberto.moncada@gmail.com>
2026-06-10 18:09:15 -0400

    Creacion de clase Preprocesador.py

---
+++ src/Preprocesador.py
@@ -0,0 +1,8 @@
+class Preprocesador:
+    def __init__(self, nombre, edad):
+        self.nombre = nombre
+        self.edad = edad
+
+    def saludar(self):
+        print(f"Hola, mi nombre es {self.nombre} y tengo {self.edad} años")
+
